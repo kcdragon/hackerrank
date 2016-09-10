4ff4287faf3527dc4f24afaed4c31f5b8f2fa23d
Mike Dalton <mdalton@weblinc.com>
2016-09-10 15:17:59 -0400

    solution for even tree

--- even_tree.rb
+++ even_tree.rb
@@ -67,12 +67,13 @@ def print_edges(edges)
 end
 
 def max_number_of_removed_edges(graph)
-  max = valid_edges_to_remove(graph).map do |edge|
+  edge = valid_edges_to_remove(graph)[0]
+  if edge
     max_number_of_removed_edges(remove(graph, *edge)) + 1
-  end.max
-  max.nil? ? 0 : max
+  else
+    0
+  end
 end
-
 
 max = nil
 require 'ruby-prof'
@@ -84,14 +85,46 @@ printer.print(STDOUT, {})
 
 puts max
 
+# __END__
+# 10 9
+# 2 1
+# 3 1
+# 4 3
+# 5 2
+# 6 1
+# 7 2
+# 8 6
+# 9 8
+# 10 8
+
 __END__
-10 9
+30 29
 2 1
-3 1
+3 2
 4 3
 5 2
-6 1
-7 2
-8 6
-9 8
-10 8
+6 4
+7 4
+8 1
+9 5
+10 4
+11 4
+12 8
+13 2
+14 2
+15 8
+16 10
+17 1
+18 17
+19 18
+20 4
+21 15
+22 20
+23 2
+24 12
+25 21
+26 17
+27 5
+28 27
+29 4
+30 25
